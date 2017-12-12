234a4e6155b92b0e1e2417c2f9bb6c9ea4da943a
Alexey Pankratov <a.pankratov@recognize.ru>
2017-12-12 18:44:25 +0300

    Oh, my english is so...

--- main.py
+++ main.py
@@ -25,48 +25,48 @@ class DoorsState(Enum):
 
 class Lift(Thread):
 
-    def __init__(self, flour_count, flour_height, speed, doors_delay):
+    def __init__(self, floor_count, floor_height, speed, doors_delay):
         super().__init__()
-        self._flour_count = flour_count
+        self._floor_count = floor_count
         self._doors_delay = doors_delay
-        self._flour_pass_time = flour_height / speed
-        self._flour = 0
+        self._floor_pass_time = floor_height / speed
+        self._floor = 0
         self._action = Action.STOP
         self._direction = Direction.UP.value
         self._doors_state = DoorsState.CLOSED
-        self._call = [0 for _ in range(flour_count)]
-        self._move = [0 for _ in range(flour_count)]
+        self._call = [0 for _ in range(floor_count)]
+        self._move = [0 for _ in range(floor_count)]
         self._btn_lock = Lock()
         self._move_lock = Lock()
         self._recalculate_needed = Event()
         self._moving = False
 
-    def call(self, flour, direction):
-        if flour < 0 or flour >= self._flour_count:
-            print('Error: Lift called to the {} flour but we have flours from 1 to {}'
-                  .format(flour + 1, self._flour_count))
+    def call(self, floor, direction):
+        if floor < 0 or floor >= self._floor_count:
+            print('Error: Lift called to the {} floor but we have floors from 1 to {}'
+                  .format(floor + 1, self._floor_count))
         else:
             with self._btn_lock:
-                self._call[flour] = direction
+                self._call[floor] = direction
             self._recalculate_needed.set()
 
-    def move(self, flour):
-        if flour < 0 or flour >= self._flour_count:
-            print('Error: Lift called to the {} flour but we have flours from 1 to {}'
-                  .format(flour + 1, self._flour_count))
+    def move(self, floor):
+        if floor < 0 or floor >= self._floor_count:
+            print('Error: Lift called to the {} floor but we have floors from 1 to {}'
+                  .format(floor + 1, self._floor_count))
         else:
             with self._btn_lock:
-                self._move[flour] = 1
+                self._move[floor] = 1
             self._recalculate_needed.set()
 
-    def _print_flour(self):
-        print('{} flour'.format(self._flour + 1))
+    def _print_floor(self):
+        print('{} floor'.format(self._floor + 1))
 
     def _recalculate_next_step(self):
         with self._move_lock, self._btn_lock:
             if self._doors_state == DoorsState.OPENED:
-                self._move[self._flour] = 0
-                self._call[self._flour] = 0
+                self._move[self._floor] = 0
+                self._call[self._floor] = 0
                 self._action = Action.CLOSE_DOORS
                 return
 
@@ -75,28 +75,28 @@ class Lift(Thread):
                 return
 
             if self._direction == Direction.UP.value and \
-                    sum(self._move[self._flour:]) + sum(self._call[self._flour + 1:]) == 0 and \
-                    self._call[self._flour] != self._direction:
+                    sum(self._move[self._floor:]) + sum(self._call[self._floor + 1:]) == 0 and \
+                    self._call[self._floor] != self._direction:
                 self._direction = Direction.DOWN.value
             elif self._direction == Direction.DOWN.value and \
-                    sum(self._move[:self._flour - 1]) + sum(self._call[:self._flour - 1]) == 0 and \
-                    self._call[self._flour] != self._direction:
+                    sum(self._move[:self._floor - 1]) + sum(self._call[:self._floor - 1]) == 0 and \
+                    self._call[self._floor] != self._direction:
                 self._direction = Direction.UP.value
 
             if self._direction == Direction.UP.value:
-                if self._move[self._flour] or self._call[self._flour] == self._direction:
+                if self._move[self._floor] or self._call[self._floor] == self._direction:
                     if self._doors_state == DoorsState.CLOSED:
                         self._action = Action.OPEN_DOORS
                 else:
-                    for i in range(self._flour + 1, self._flour_count):
+                    for i in range(self._floor + 1, self._floor_count):
                         if self._move[i] or self._call[i] == self._direction:
                             self._action = Action.UP
             elif self._direction == Direction.DOWN.value:
-                if self._move[self._flour] or self._call[self._flour] == self._direction:
+                if self._move[self._floor] or self._call[self._floor] == self._direction:
                     if self._doors_state == DoorsState.CLOSED:
                         self._action = Action.OPEN_DOORS
                 else:
-                    for i in range(self._flour - 1, 0, -1):
+                    for i in range(self._floor - 1, 0, -1):
                         if self._move[i] or self._call[i] == self._direction:
                             self._action = Action.DOWN
 
@@ -107,16 +107,16 @@ class Lift(Thread):
                 start_time = time()
                 if start_time >= next_time:
                     if self._moving:
-                        self._print_flour()
+                        self._print_floor()
                     with self._move_lock:
                         if self._action == Action.DOWN:
                             self._moving = True
-                            self._flour -= 1
-                            next_time += self._flour_pass_time
+                            self._floor -= 1
+                            next_time += self._floor_pass_time
                         elif self._action == Action.UP:
                             self._moving = True
-                            self._flour += 1
-                            next_time += self._flour_pass_time
+                            self._floor += 1
+                            next_time += self._floor_pass_time
                         elif self._action == Action.OPEN_DOORS:
                             self._moving = False
                             print('Doors opened')
@@ -141,34 +141,34 @@ class Lift(Thread):
 def main():
     args = sys.argv[1:]
     try:
-        opts, args = getopt.getopt(args, 'c:f:s:d:', ['flour_count=', 'flour_height=', 'speed=', 'doors_delay='])
+        opts, args = getopt.getopt(args, 'c:f:s:d:', ['floor_count=', 'floor_height=', 'speed=', 'doors_delay='])
     except getopt.GetoptError:
-        print('usage: python {} -c <flour_count> -f <flour_height> -s <speed> -d <doors_delay>'.format(sys.argv[0]))
+        print('usage: python {} -c <floor_count> -f <floor_height> -s <speed> -d <doors_delay>'.format(sys.argv[0]))
         sys.exit(2)
     if len(opts) != 4:
-        print('usage: python {} -c <flour_count> -f <flour_height> -s <speed> -d <doors_delay>'.format(sys.argv[0]))
+        print('usage: python {} -c <floor_count> -f <floor_height> -s <speed> -d <doors_delay>'.format(sys.argv[0]))
         sys.exit(2)
-    flour_count = 0
-    flour_height = 0
+    floor_count = 0
+    floor_height = 0
     speed = 0
     doors_delay = 0
     for opt, arg in opts:
-        if opt in ['-c', '--flour_count']:
+        if opt in ['-c', '--floor_count']:
             try:
                 if int(arg) < 5 or int(arg) > 20:
-                    print('Error: Incorrect flour count. Must be integer from 5 to 20.')
+                    print('Error: Incorrect floor count. Must be integer from 5 to 20.')
                     sys.exit(2)
-                flour_count = int(arg)
+                floor_count = int(arg)
             except ValueError:
-                print('Error: Incorrect flour count. Must be integer from 5 to 20.')
-        if opt in ['-f', '--flour_height']:
+                print('Error: Incorrect floor count. Must be integer from 5 to 20.')
+        if opt in ['-f', '--floor_height']:
             try:
                 if float(arg) <= 0:
-                    print('Error: Incorrect flour height. Must be float value greater then 0.')
+                    print('Error: Incorrect floor height. Must be float value greater then 0.')
                     sys.exit(2)
-                flour_height = float(arg)
+                floor_height = float(arg)
             except ValueError:
-                print('Error: Incorrect flour height. Must be float value greater then 0.')
+                print('Error: Incorrect floor height. Must be float value greater then 0.')
         if opt in ['-s', '--speed']:
             try:
                 if float(arg) <= 0:
@@ -185,18 +185,18 @@ def main():
                 doors_delay = float(arg)
             except ValueError:
                 print('Error: Incorrect doors delay. Must be float value greater then 0.')
-    lift = Lift(flour_count, flour_height, speed, doors_delay)
+    lift = Lift(floor_count, floor_height, speed, doors_delay)
     lift.start()
     try:
         while True:
             try:
-                order, flour = input().split()
+                order, floor = input().split()
                 if order == 'm':
-                    lift.move(int(flour) - 1)
+                    lift.move(int(floor) - 1)
                 elif order == 'cu':
-                    lift.call(int(flour) - 1, Direction.UP.value)
+                    lift.call(int(floor) - 1, Direction.UP.value)
                 elif order == 'cd':
-                    lift.call(int(flour) - 1, Direction.DOWN.value)
+                    lift.call(int(floor) - 1, Direction.DOWN.value)
             except ValueError:
                 pass
     except KeyboardInterrupt:
